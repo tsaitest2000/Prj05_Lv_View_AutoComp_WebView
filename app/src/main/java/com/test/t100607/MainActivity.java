851b788ffd6591c5b4706c7c4168b9b0763b7218
tsaitest2000 <tsaitest2000@gmail.com>
2016-10-11 23:17:36 +0800

    Prj05_Lv_View_AutoComp_WebView 全部模組

--- app/src/main/java/com/test/t100607/MainActivity.java
+++ app/src/main/java/com/test/t100607/MainActivity.java
@@ -1,11 +1,10 @@
 package com.test.t100607;
 
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Toast;
 
